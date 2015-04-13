46eb09d523306c60f55a282368382db95b276865
Max Conway <conway.max1@gmail.com>
2015-04-13 16:02:04 +0100

    changes to prediction script for cross validation

--- scripts/prediction_script.R
+++ scripts/prediction_script.R
@@ -8,41 +8,52 @@ library(magrittr)
 RMSE <- function(predicted, actual){
   sqrt(mean((predicted-actual)^2,na.rm=TRUE))
 }
-logical2string <- function(logical){
-  ifelse(logical, 'yes', 'no')
+logical2factor <- function(logical){
+  ifelse(logical, 'yes', 'no') %>% factor(levels=c('yes','no'))
 }
 
 
 data <- read_csv('data/train.csv')
 
 preprocess <- function(x){x %>%
-                 mutate(`Open Date` = dmy(`Open Date`),
-                        month = month(`Open Date`),
-                        has_time = logical2string(`Open Date`!=floor_date(`Open Date`,'day')),
-                        in_Istanbul = logical2string(City=='İstanbul'),
-                        Type = factor(Type, levels=c('FC', 'IL', 'DT', 'MB'))
-                 )}
+                            mutate(`Open Date` = dmy(`Open Date`),
+                                   month = month(`Open Date`),
+                                   has_time = logical2factor(`Open Date`!=floor_date(`Open Date`,'day')),
+                                   in_Istanbul = logical2string(City=='İstanbul'),
+                                   Type = factor(Type, levels=c('FC', 'IL', 'DT', 'MB')),
+                                   `City Group` = factor(`City Group`, levels=c('Big Cities', 'Other'))
+                            )}
 
 data <- data %>% preprocess()
 real_train <- data
-  
-assignment <- sample.int(nrow(data),0.9*nrow(data))
-train <- data %>% filter(row_number() %in% assignment)
-test <- data %>% filter(!(row_number() %in% assignment))
+
+crossvalidate <- function(modelfun, data, iterations=10, sample_ratio=1/nrow(data)){
+  laply(1:iterations, function(ignored){
+    assignment <- sample.int(nrow(data),sample_ratio*nrow(data))
+    train <- data %>% filter(!(row_number() %in% assignment))
+    test <- data %>% filter((row_number() %in% assignment))
+    mod <- modelfun(train)
+    return(RMSE(predict(mod, test),test$revenue))
+  })
+}
 
 
 # rpart prediction
 library(rpart)
-mod <- train %>% select(-Id,-City) %>% rpart(revenue ~ ., .)
-RMSE(predict(mod, test),test$revenue)
+modelfun <- function(train){
+  train %>% select(-Id,-City) %>% rpart(revenue ~ ., .)
+}
+crossvalidate(modelfun, data)
 
 usedmod <- real_train %>% select(-Id,-City) %>% rpart(revenue ~ ., .)
 
 # Cubist prediction
 library(Cubist)
-mod <- cubist(x = train %>% select(-Id, -revenue, -`Open Date`, -City),
-              y = train$revenue)
-RMSE(predict(mod, test),test$revenue)
+modelfun <- function(train){
+  cubist(x = train %>% select(-Id, -revenue, -`Open Date`, -City),
+         y = train$revenue)
+}
+crossvalidate(modelfun, data)
 
 
 # usedmod <- cubist(x = real_train %>% select(-Id, -revenue, -`Open Date`, -City),
@@ -55,5 +66,5 @@ RMSE(predict(mod, test),test$revenue)
 real_test <- read_csv('data/test.csv') %>% preprocess
 submission <- data_frame(Id = real_test$Id,
                          Prediction = predict(usedmod, real_test)
-                         )
+)
 write_csv(submission, paste0('out/','submission',Sys.Date() %>% format('%FT%H:%M:%S'),'.csv'))
